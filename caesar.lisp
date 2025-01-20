e5694f176d195445ae4eee9fa2eb9a2b56e1175d
theKyrylo <zhulin.pers0n4l@gmail.com>
2025-01-20 11:11:20 +0100

    The new way to encode and decode

--- caesar.lisp
+++ caesar.lisp
@@ -1,26 +1,16 @@
-(defparameter *alphabet* "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ")
+(defvar *alphabet* "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ")
 
-(defun %shift-alphabet (alphabet offset)
-  (concatenate 'string
-                   (subseq alphabet (- (length alphabet) offset)) ; should I put this length to let??
-                   (subseq alphabet 0 (- (length alphabet) offset))))
+(defun caesar-encode (string offset &optional (alphabet *alphabet*))
+  (with-output-to-string (stream)
+    (loop for char across string
+          for position = (position char alphabet :test #'char-equal)
+          if (null position)
+            do (write-char char stream)
+          else do
+            (let* ((new-position (mod (- position offset) (length alphabet)))
+                   (new-char (char alphabet new-position))
+                   (new-char (if (upper-case-p char) new-char (char-downcase new-char))))
+              (write-char new-char stream)))))
 
-(defun %caesar-helper (phrase alphabet new-alphabet collector)
-  (loop
-    for char across (string-upcase phrase)
-    do (when (find char alphabet)
-         (setf collector
-               (concatenate
-                'string
-                collector (subseq new-alphabet
-                               (position char alphabet)
-                               (+ 1 (position char alphabet))))))
-    finally (return-from %caesar-helper collector)))
-
-(defun caesar-encrypt (phrase offset)
-  (let ((new-alphabet (%shift-alphabet *alphabet* offset)))
-    (%caesar-helper phrase *alphabet* new-alphabet "")))
-
-(defun caesar-decrypt (phrase offset)
-  (let ((old-alphabet (%shift-alphabet *alphabet* offset)))
-    (%caesar-helper phrase old-alphabet *alphabet* "")))
+(defun caesar-decode (string offset &optional (alphabet *alphabet*))
+  (caesar-encode string (- offset) alphabet))
