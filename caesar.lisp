922d2cfbe8ea396156e5860693193c86d6eae28f
theKyrylo <zhulin.pers0n4l@gmail.com>
2025-01-20 12:02:40 +0100

    :ignore-other-symbols and :ignore-case added

--- caesar.lisp
+++ caesar.lisp
@@ -1,16 +1,20 @@
 (defvar *alphabet* "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ")
 
-(defun caesar-encode (string offset &optional (alphabet *alphabet*))
+(defun caesar-encode (string offset &key (alphabet *alphabet*) ignore-other-symbols ignore-case)
   (with-output-to-string (stream)
     (loop for char across string
           for position = (position char alphabet :test #'char-equal)
           if (null position)
-            do (write-char char stream)
+            do (if (null ignore-other-symbols)
+                   (write-char char stream))
           else do
             (let* ((new-position (mod (- position offset) (length alphabet)))
                    (new-char (char alphabet new-position))
-                   (new-char (if (upper-case-p char) new-char (char-downcase new-char))))
+                   (new-char (if (and (upper-case-p char) (null ignore-case))
+                                 new-char (char-downcase new-char))))
               (write-char new-char stream)))))
 
-(defun caesar-decode (string offset &optional (alphabet *alphabet*))
-  (caesar-encode string (- offset) alphabet))
+(defun caesar-decode (string offset &key (alphabet *alphabet*))
+  (caesar-encode string (- offset) :alphabet alphabet
+                                   :ignore-other-symbols nil
+                                   :ignore-case nil))
